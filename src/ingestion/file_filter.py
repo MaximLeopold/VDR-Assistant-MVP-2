efabd222d4deefb24b58a157c2c7ca2522e4ddd2
MaximLeopold <maxim.luetzenrath@outlook.com>
2026-06-30 14:42:56 +0200

    Create file_filter.py

---
+++ src/ingestion/file_filter.py
@@ -0,0 +1,24 @@
+"""Filter supported and unsupported files.
+
+Responsibilities:
+
+- Accept supported document types.
+- Ignore unsupported file types.
+- Ignore temporary system files.
+- Report skipped files.
+
+Initial supported file types:
+
+- PDF
+- PPTX
+- DOCX
+
+Initial ignored file types:
+
+- XLSX
+- XLS
+- XLSM
+- CSV
+
+Excel support will be added in a later version.
+"""
