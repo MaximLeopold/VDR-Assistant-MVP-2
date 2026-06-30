952bca6c4a3eda9da71f8953e4becc8b313d9da3
MaximLeopold <maxim.luetzenrath@outlook.com>
2026-06-30 14:12:24 +0200

    Create summarize_chain.py

---
+++ src/chains/summarize_chain.py
@@ -0,0 +1,5 @@
+"""Summarize chain for summarizing VDR documents or topics.
+
+This is planned for a later phase and should only summarize information
+retrieved from the active VDR vector store.
+"""
